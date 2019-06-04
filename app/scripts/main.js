d30475c09bc493593747c6be12f626041fcbf4cf
sonejka <kseniya.varabei@gmail.com>
2019-06-04 14:57:35 +0200

    add: schedule for js weekend

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,12 @@
-$(window).on('scroll touchmove', function () {
+$(window).on('scroll touchmove', function() {
 
   if ($('.menu-btn:checked').length === 0) {
 
     let coloredSections = $('section[data-color]');
 
-    const scrollTop = $(document).scrollTop() + $(window).height()/2;
+    const scrollTop = $(document).scrollTop() + $(window).height() / 2;
 
-    coloredSections.each(function (sectionIndex, sec) {
+    coloredSections.each(function(sectionIndex, sec) {
       let section = $(sec);
       if (scrollTop >= section.position().top) {
         $('.page-wrap').css('background', section.attr('data-color'));
@@ -16,25 +16,40 @@ $(window).on('scroll touchmove', function () {
 });
 
 
-$(document).ready( function () {
-  $('.menu-btn').click(function () {
-    $('body').toggleClass('noscroll') ;
-  })
+$(document).ready(function() {
+  $('.menu-btn').click(function() {
+    $('body').toggleClass('noscroll');
+  });
+
+
+  let dayToShow = window.location.hash;
+
+  if (dayToShow === '') {
+    dayToShow = '#js-sat';
+  }
+
+  dayToShow = dayToShow.substr(1);
+  $('.schedule-tabs > .btn').removeClass('active');
+  $('.schedule-tabs > .btn[data-day="' + dayToShow + '"]').addClass('active');
+  $('.schedule').removeClass('active');
+  let selector = '.schedule.' + dayToShow;
+  $(selector).addClass('active');
+
 
 // schedule tabs
-  $('.first-day-btn').click(function () {
-    $('.first-day').addClass('active');
-    $('.first-day-btn').addClass('active');
-    $('.second-day').removeClass('active');
-    $('.second-day-btn').removeClass('active');
-  })
 
-  $('.second-day-btn').click(function () {
-    $('.second-day').addClass('active');
-    $('.second-day-btn').addClass('active');
-    $('.first-day').removeClass('active');
-    $('.first-day-btn').removeClass('active');
-  })
+  $('.schedule-tabs > .btn').click(function(event) {
+    event.preventDefault();
 
-})
+    window.location.hash = $(this).data('day');
+    $('.schedule-tabs > .btn').removeClass('active');
+    $(this).addClass('active');
+    $('.schedule').removeClass('active');
+
+    let selector = '.schedule.' + $(this).data('day');
+    $(selector).addClass('active');
+  });
+
+
+});
 
