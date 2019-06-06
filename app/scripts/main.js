4707e28db8f5f039057cb3bebfb699ad30ae1878
sonejka <kseniya.varabei@gmail.com>
2019-06-06 14:28:49 +0200

    upd: hashtag for mobile people day

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
   let dayToShow = window.location.hash;
 
   if (dayToShow === '') {
-    dayToShow = '#mobile-people-friday';
+    dayToShow = '#mobile-people-day';
   }
 
   dayToShow = dayToShow.substr(1);
