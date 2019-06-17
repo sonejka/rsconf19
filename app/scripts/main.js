b4636894ef63427c20f01a74b08e284206929f90
sonejka <kseniya.varabei@gmail.com>
2019-06-17 14:05:35 +0200

    add: scroll top btn

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -51,5 +51,20 @@ $(document).ready(function() {
   });
 
 
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.scroll-top').fadeIn();
+    } else {
+      $('.scroll-top').fadeOut();
+    }
+  });
+
+  $('.scroll-top').click(function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 100);
+    return false;
+  });
 });
+
 
